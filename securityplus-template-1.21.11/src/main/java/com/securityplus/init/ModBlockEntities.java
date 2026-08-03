36de12bd242b8777fe4e16e6285ba8fde06bf8e4
MontaraGroup <contact@skimanager.net>
2026-08-03 19:12:25 -0400

    Update ModBlockEntities.java

--- securityplus-template-1.21.11/src/main/java/com/securityplus/init/ModBlockEntities.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/init/ModBlockEntities.java
@@ -1,56 +1,41 @@
 package com.securityplus.init;
 
-import com.securityplus.blockentity.KeypadBlockEntity;
-import com.securityplus.blockentity.LockdownLeverBlockEntity;
-import com.securityplus.blockentity.OwnableBlockEntity;
-import com.securityplus.blockentity.SentryTurretBlockEntity;
-import net.minecraft.block.entity.BlockEntity;
+import com.securityplus.blockentity.*;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModBlockEntities {
+    public static BlockEntityType<OwnableBlockEntity> OWNABLE_BLOCK_ENTITY;
+    public static BlockEntityType<KeypadBlockEntity> KEYPAD_BLOCK_ENTITY;
+    public static BlockEntityType<LockdownLeverBlockEntity> LOCKDOWN_LEVER_BLOCK_ENTITY;
+    public static BlockEntityType<SentryTurretBlockEntity> SENTRY_TURRET_BLOCK_ENTITY;
 
-    public static final BlockEntityType<OwnableBlockEntity> OWNABLE_BLOCK_ENTITY = register(
-            "ownable_block_entity",
-            BlockEntityType.Builder.create(
-                    OwnableBlockEntity::new,
-                    ModBlocks.REINFORCED_STONE,
-                    ModBlocks.REINFORCED_OBSIDIAN,
-                    ModBlocks.REINFORCED_IRON_BARS
-            ).build()
-    );
+    public static void registerBlockEntities() {
+        OWNABLE_BLOCK_ENTITY = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier.of("securityplus", "ownable"),
+            FabricBlockEntityTypeBuilder.create(OwnableBlockEntity::new, ModBlocks.REINFORCED_BLOCK).build()
+        );
 
-    public static final BlockEntityType<KeypadBlockEntity> KEYPAD_BLOCK_ENTITY = register(
-            "keypad_block_entity",
-            BlockEntityType.Builder.create(
-                    KeypadBlockEntity::new,
-                    ModBlocks.KEYPAD
-            ).build()
-    );
+        KEYPAD_BLOCK_ENTITY = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier.of("securityplus", "keypad"),
+            FabricBlockEntityTypeBuilder.create(KeypadBlockEntity::new, ModBlocks.KEYPAD_BLOCK).build()
+        );
 
-    public static final BlockEntityType<LockdownLeverBlockEntity> LOCKDOWN_LEVER_BLOCK_ENTITY = register(
-            "lockdown_lever_block_entity",
-            BlockEntityType.Builder.create(
-                    LockdownLeverBlockEntity::new,
-                    ModBlocks.LOCKDOWN_LEVER
-            ).build()
-    );
+        LOCKDOWN_LEVER_BLOCK_ENTITY = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier.of("securityplus", "lockdown_lever"),
+            FabricBlockEntityTypeBuilder.create(LockdownLeverBlockEntity::new, ModBlocks.LOCKDOWN_LEVER_BLOCK).build()
+        );
 
-    public static final BlockEntityType<SentryTurretBlockEntity> SENTRY_TURRET_BLOCK_ENTITY = register(
-            "sentry_turret_block_entity",
-            BlockEntityType.Builder.create(
-                    SentryTurretBlockEntity::new,
-                    ModBlocks.SENTRY_TURRET
-            ).build()
-    );
-
-    private static <T extends BlockEntity> BlockEntityType<T> register(String name, BlockEntityType<T> type) {
-        return Registry.register(Registries.BLOCK_ENTITY_TYPE, Identifier.of("securityplus", name), type);
-    }
-
-    public static void registerModBlockEntities() {
-        // Triggers static initialization
+        SENTRY_TURRET_BLOCK_ENTITY = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier.of("securityplus", "sentry_turret"),
+            FabricBlockEntityTypeBuilder.create(SentryTurretBlockEntity::new, ModBlocks.SENTRY_TURRET_BLOCK).build()
+        );
     }
 }
