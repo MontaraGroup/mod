dd97a7c1b591bd54cd918901740b3643c0296007
MontaraGroup <contact@skimanager.net>
2026-08-03 18:46:25 -0400

    Refactor LaserGridBlock to use Block class

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LaserGridBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LaserGridBlock.java
@@ -1,87 +1,61 @@
 package com.securityplus.blocks;
 
-import java.util.List;
-import net.minecraft.class_1309;
-import net.minecraft.class_1922;
-import net.minecraft.class_1937;
-import net.minecraft.class_2248;
-import net.minecraft.class_2338;
-import net.minecraft.class_2350;
-import net.minecraft.class_238;
-import net.minecraft.class_2680;
-import net.minecraft.class_2689;
-import net.minecraft.class_2741;
-import net.minecraft.class_2746;
-import net.minecraft.class_2769;
-import net.minecraft.class_3218;
-import net.minecraft.class_4970;
-import net.minecraft.class_5819;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
-public class LaserGridBlock extends class_2248 {
-   public static final class_2746 POWERED;
+public class LaserGridBlock extends Block {
+    public static final BooleanProperty POWERED = Properties.POWERED;
 
-   public LaserGridBlock(class_4970.class_2251 var1) {
-      super(var1);
-      this.registerDefaultState((class_2680)((class_2680)this.stateDefinition.any()).setValue(POWERED, false));
-   }
+    public LaserGridBlock(AbstractBlock.Settings settings) {
+        super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
+    }
 
-   protected void createBlockStateDefinition(class_2689.class_2690<class_2248, class_2680> var1) {
-      var1.add(new class_2769[]{POWERED});
-   }
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(POWERED);
+    }
 
-   public boolean isSignalSource(class_2680 var1) {
-      return true;
-   }
+    @Override
+    public boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
 
-   public int getSignal(class_2680 var1, class_1922 var2, class_2338 var3, class_2350 var4) {
-      return (Boolean)var1.getValue(POWERED) ? 15 : 0;
-   }
+    @Override
+    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return state.get(POWERED) ? 15 : 0;
+    }
 
-   public void onPlace(class_2680 var1, class_1937 var2, class_2338 var3, class_2680 var4, boolean var5) {
-      if (!var2.isClientSide()) {
-         var2.scheduleTick(var3, this, 4);
-      }
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
+        super.onPlaced(world, pos, state, placer, itemStack);
+    }
 
-   }
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (state.get(POWERED)) {
+            world.setBlockState(pos, state.with(POWERED, false), Block.NOTIFY_ALL);
+        }
+    }
 
-   public void tick(class_2680 var1, class_3218 var2, class_2338 var3, class_5819 var4) {
-      boolean var5 = false;
-
-      for(class_2350 var9 : class_2350.values()) {
-         for(int var10 = 1; var10 <= 10; ++var10) {
-            class_2338 var11 = var3.relative(var9, var10);
-            if (var2.getBlockState(var11).is(this)) {
-               double var12 = (double)Math.min(var3.getX(), var11.getX());
-               double var14 = (double)Math.min(var3.getY(), var11.getY());
-               double var16 = (double)Math.min(var3.getZ(), var11.getZ());
-               double var18 = (double)Math.max(var3.getX(), var11.getX()) + (double)1.0F;
-               double var20 = (double)Math.max(var3.getY(), var11.getY()) + (double)1.0F;
-               double var22 = (double)Math.max(var3.getZ(), var11.getZ()) + (double)1.0F;
-               class_238 var24 = new class_238(var12, var14, var16, var18, var20, var22);
-               List var25 = var2.getEntitiesOfClass(class_1309.class, var24);
-               if (var25.isEmpty()) {
-                  break;
-               }
-
-               var5 = true;
-
-               for(class_1309 var27 : var25) {
-                  var27.hurt(var2.damageSources().inFire(), 3.0F);
-               }
-               break;
-            }
-         }
-      }
-
-      if ((Boolean)var1.getValue(POWERED) != var5) {
-         var2.setBlock(var3, (class_2680)var1.setValue(POWERED, var5), 3);
-         var2.updateNeighborsAt(var3, this);
-      }
-
-      var2.scheduleTick(var3, this, 4);
-   }
-
-   static {
-      POWERED = class_2741.field_12521;
-   }
+    @Override
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (!world.isClient && !state.get(POWERED)) {
+            world.setBlockState(pos, state.with(POWERED, true), Block.NOTIFY_ALL);
+            world.scheduleBlockTick(pos, this, 20);
+        }
+    }
 }
