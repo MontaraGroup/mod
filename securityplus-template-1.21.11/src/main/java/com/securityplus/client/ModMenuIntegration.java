480d02b9771cf564aa23202fd6f6d2690567c860
MontaraGroup <contact@skimanager.net>
2026-08-03 19:14:52 -0400

    Refactor getModConfigScreenFactory method

--- securityplus-template-1.21.11/src/main/java/com/securityplus/client/ModMenuIntegration.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/client/ModMenuIntegration.java
@@ -2,11 +2,12 @@ package com.securityplus.client;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import java.lang.reflect.Proxy;
-import net.minecraft.class_437;
+import net.minecraft.client.gui.screen.Screen;
 
 public class ModMenuIntegration implements ModMenuApi {
-   public ConfigScreenFactory<?> getModConfigScreenFactory() {
-      return (ConfigScreenFactory)Proxy.newProxyInstance(ModMenuApi.class.getClassLoader(), new Class[]{ConfigScreenFactory.class}, (var0, var1, var2) -> "create".equals(var1.getName()) && var2 != null && var2.length == 1 ? new SecurityConfigScreen((class_437)var2[0]) : null);
-   }
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return SecurityConfigScreen::new;
+    }
 }
