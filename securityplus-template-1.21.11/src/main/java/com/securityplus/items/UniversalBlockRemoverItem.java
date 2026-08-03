99ce9601e2ff8da5d22f761c86b89503fa0898e3
MontaraGroup <contact@skimanager.net>
2026-08-03 19:08:04 -0400

    Refactor UniversalBlockRemoverItem to use modern API

--- securityplus-template-1.21.11/src/main/java/com/securityplus/items/UniversalBlockRemoverItem.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/items/UniversalBlockRemoverItem.java
@@ -1,39 +1,38 @@
 package com.securityplus.items;
 
 import com.securityplus.blockentity.OwnableBlockEntity;
-import net.minecraft.class_1269;
-import net.minecraft.class_1657;
-import net.minecraft.class_1792;
-import net.minecraft.class_1838;
-import net.minecraft.class_1937;
-import net.minecraft.class_2338;
-import net.minecraft.class_2561;
-import net.minecraft.class_2586;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class UniversalBlockRemoverItem extends class_1792 {
-   public UniversalBlockRemoverItem(class_1792.class_1793 var1) {
-      super(var1);
-   }
+public class UniversalBlockRemoverItem extends Item {
 
-   public class_1269 useOn(class_1838 var1) {
-      class_1937 var2 = var1.getLevel();
-      class_2338 var3 = var1.getClickedPos();
-      class_1657 var4 = var1.getPlayer();
-      if (!var2.isClientSide() && var4 != null) {
-         class_2586 var5 = var2.getBlockEntity(var3);
-         if (var5 instanceof OwnableBlockEntity) {
-            OwnableBlockEntity var6 = (OwnableBlockEntity)var5;
-            if (!var6.isOwnedBy(var4) && !var4.isCreative()) {
-               var4.displayClientMessage(class_2561.literal("§cOnly the owner (" + var6.getOwnerName() + ") can use Universal Block Remover!"), true);
-               return class_1269.FAIL;
+    public UniversalBlockRemoverItem(Item.Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        BlockPos pos = context.getBlockPos();
+        PlayerEntity player = context.getPlayer();
+
+        if (!world.isClient() && player != null) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+            if (blockEntity instanceof OwnableBlockEntity ownable) {
+                if (ownable.isOwnedBy(player) || player.hasPermissionLevel(2)) {
+                    world.breakBlock(pos, true, player);
+                    return ActionResult.SUCCESS;
+                } else {
+                    player.sendMessage(Text.literal("You do not own this block!"), true);
+                }
             }
-
-            var2.destroyBlock(var3, true, var4);
-            var4.displayClientMessage(class_2561.literal("§aReinforced block deconstructed."), true);
-            return class_1269.SUCCESS;
-         }
-      }
-
-      return super.useOn(var1);
-   }
+        }
+        return ActionResult.PASS;
+    }
 }
