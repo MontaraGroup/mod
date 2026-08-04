eb3a7d600d7ebc50565ce4259d1b814284439c5a
MontaraGroup <contact@skimanager.net>
2026-08-03 20:07:43 -0400

    Refactor LockdownDoorBlock class
    Refactor LockdownDoorBlock to simplify state management and add door interaction logic.

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownDoorBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownDoorBlock.java
@@ -1,35 +1,23 @@
 package com.securityplus.blocks;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class LockdownDoorBlock extends Block {
-    public static final BooleanProperty POWERED = Properties.POWERED;
-    public static final BooleanProperty OPEN = Properties.OPEN;
-
-    public LockdownDoorBlock(AbstractBlock.Settings settings) {
+    public LockdownDoorBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false).with(OPEN, false));
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED, OPEN);
-    }
-
-    @Override
-    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         if (!world.isClient()) {
-            boolean isPowered = world.isReceivingRedstonePower(pos);
-            if (isPowered != state.get(POWERED)) {
-                world.setBlockState(pos, state.with(POWERED, isPowered).with(OPEN, isPowered), Block.NOTIFY_ALL);
-            }
+            // Door interaction logic
         }
+        return ActionResult.SUCCESS;
     }
 }
