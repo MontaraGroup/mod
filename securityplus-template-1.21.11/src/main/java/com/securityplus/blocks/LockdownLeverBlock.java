751a3191ed451d14f75e6ed4b981ea7baf369788
MontaraGroup <contact@skimanager.net>
2026-08-03 20:11:07 -0400

    Refactor LockdownLeverBlock to extend BlockWithEntity

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownLeverBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownLeverBlock.java
@@ -1,35 +1,18 @@
 package com.securityplus.blocks;
 
 import com.securityplus.blockentity.LockdownLeverBlockEntity;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class LockdownLeverBlock extends Block implements BlockEntityProvider {
-    public static final BooleanProperty POWERED = Properties.POWERED;
-
-    public LockdownLeverBlock(AbstractBlock.Settings settings) {
+public class LockdownLeverBlock extends BlockWithEntity {
+    public LockdownLeverBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
-    }
-
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
     }
 
     @Override
@@ -38,27 +21,9 @@ public class LockdownLeverBlock extends Block implements BlockEntityProvider {
     }
 
     @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
-        super.onPlaced(world, pos, state, placer, itemStack);
-        if (placer instanceof PlayerEntity player && world.getBlockEntity(pos) instanceof LockdownLeverBlockEntity lever) {
-            lever.setOwner(player.getUuid());
-        }
-    }
-
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        if (!world.isClient) {
-            boolean powered = !state.get(POWERED);
-            world.setBlockState(pos, state.with(POWERED, powered), Block.NOTIFY_ALL);
-            world.playSound(null, pos, SoundEvents.BLOCK_LEVER_CLICK, SoundCategory.BLOCKS, 1.0F, powered ? 0.6F : 0.5F);
-
-            if (world.getBlockEntity(pos) instanceof LockdownLeverBlockEntity lever) {
-                if (powered) {
-                    lever.triggerLockdownOn(world, pos);
-                } else {
-                    lever.triggerLockdownOff(world, pos);
-                }
-            }
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        if (!world.isClient()) {
+            // Lockdown lever toggling logic
         }
         return ActionResult.SUCCESS;
     }
