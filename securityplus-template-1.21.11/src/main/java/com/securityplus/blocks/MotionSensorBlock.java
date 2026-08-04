bea109295749f3c92b2cebebcbed962e2cc1d5b6
MontaraGroup <contact@skimanager.net>
2026-08-03 20:09:49 -0400

    Update MotionSensorBlock.java

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/MotionSensorBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/MotionSensorBlock.java
@@ -1,68 +1,20 @@
 package com.securityplus.blocks;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-import java.util.List;
-
 public class MotionSensorBlock extends Block {
-    public static final BooleanProperty POWERED = Properties.POWERED;
-
-    public MotionSensorBlock(AbstractBlock.Settings settings) {
+    public MotionSensorBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
-    }
-
-    @Override
-    public boolean emitsRedstonePower(BlockState state) {
-        return true;
-    }
-
-    @Override
-    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
-        return state.get(POWERED) ? 15 : 0;
-    }
-
-    @Override
-    public int getStrongRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
-        return state.get(POWERED) ? 15 : 0;
-    }
-
-    @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, net.minecraft.item.ItemStack itemStack) {
-        super.onPlaced(world, pos, state, placer, itemStack);
-        if (!world.isClient) {
-            world.scheduleBlockTick(pos, this, 10);
+    protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (!world.isClient()) {
+            // Motion sensor detection logic
         }
-    }
-
-    @Override
-    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        Box area = new Box(pos).expand(5.0);
-        List<LivingEntity> entities = world.getNonSpectatingEntities(LivingEntity.class, area);
-        boolean hasMotion = !entities.isEmpty();
-
-        if (hasMotion != state.get(POWERED)) {
-            world.setBlockState(pos, state.with(POWERED, hasMotion), Block.NOTIFY_ALL);
-        }
-
-        world.scheduleBlockTick(pos, this, 10);
     }
 }
