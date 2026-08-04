74ec8eafea1614d468adcd238b0854c5bad7d41a
MontaraGroup <contact@skimanager.net>
2026-08-03 21:01:45 -0400

    Refactor KeypadBlockEntity passcode management
    Updated passcode initialization and improved NBT handling for passcode.

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blockentity/KeypadBlockEntity.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blockentity/KeypadBlockEntity.java
@@ -1,37 +1,42 @@
 package com.securityplus.blockentity;
 
+import com.securityplus.init.ModBlockEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.math.BlockPos;
 
 public class KeypadBlockEntity extends OwnableBlockEntity {
-    private String passcode = "";
+    private String passcode = "1234";
 
     public KeypadBlockEntity(BlockPos pos, BlockState state) {
-        super(pos, state);
+        super(ModBlockEntities.KEYPAD_BLOCK_ENTITY, pos, state);
+    }
+
+    public void setPasscode(String passcode) {
+        this.passcode = passcode != null ? passcode : "";
+        this.markDirty();
+    }
+
+    public String getPasscode() {
+        return this.passcode;
+    }
+
+    public boolean verifyPasscode(String passcode) {
+        return this.passcode.equals(passcode);
     }
 
     @Override
     protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
         super.readNbt(nbt, registries);
-        this.passcode = nbt.getString("Passcode").orElse("");
+        if (nbt.contains("Passcode")) {
+            this.passcode = nbt.getString("Passcode");
+        }
     }
 
     @Override
     protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
         super.writeNbt(nbt, registries);
-        if (this.passcode != null) {
-            nbt.putString("Passcode", this.passcode);
-        }
-    }
-
-    public String getPasscode() {
-        return passcode;
-    }
-
-    public void setPasscode(String passcode) {
-        this.passcode = passcode;
-        markDirty();
+        nbt.putString("Passcode", this.passcode);
     }
 }
