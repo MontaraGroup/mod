f17bca8355097d3063a8f5479edb1f06f785e339
MontaraGroup <contact@skimanager.net>
2026-08-03 20:09:18 -0400

    Refactor LaserGridBlock and remove unused methods

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LaserGridBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LaserGridBlock.java
@@ -1,61 +1,23 @@
 package com.securityplus.blocks;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 public class LaserGridBlock extends Block {
     public static final BooleanProperty POWERED = Properties.POWERED;
 
-    public LaserGridBlock(AbstractBlock.Settings settings) {
+    public LaserGridBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
-    }
-
-    @Override
-    public boolean emitsRedstonePower(BlockState state) {
-        return true;
-    }
-
-    @Override
-    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
-        return state.get(POWERED) ? 15 : 0;
-    }
-
-    @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
-        super.onPlaced(world, pos, state, placer, itemStack);
-    }
-
-    @Override
-    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (state.get(POWERED)) {
-            world.setBlockState(pos, state.with(POWERED, false), Block.NOTIFY_ALL);
-        }
-    }
-
-    @Override
-    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        if (!world.isClient && !state.get(POWERED)) {
-            world.setBlockState(pos, state.with(POWERED, true), Block.NOTIFY_ALL);
-            world.scheduleBlockTick(pos, this, 20);
+    protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
+        if (!world.isClient() && !state.get(POWERED)) {
+            // Laser grid redstone logic
         }
     }
 }
