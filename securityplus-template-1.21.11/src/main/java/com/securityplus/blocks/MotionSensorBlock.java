87a483125534e7e1eb18a4889123fcd5280d04cb
MontaraGroup <contact@skimanager.net>
2026-08-03 21:00:33 -0400

    Implement motion sensor functionality with redstone power

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/MotionSensorBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/MotionSensorBlock.java
@@ -2,19 +2,66 @@ package com.securityplus.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 public class MotionSensorBlock extends Block {
+    public static final BooleanProperty POWERED = Properties.POWERED;
+
     public MotionSensorBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getStateManager().getDefaultState().with(POWERED, false));
     }
 
     @Override
-    protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(POWERED);
+    }
+
+    @Override
+    protected boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
+
+    @Override
+    protected int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return state.get(POWERED) ? 15 : 0;
+    }
+
+    @Override
+    protected int getStrongRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return state.get(POWERED) ? 15 : 0;
+    }
+
+    @Override
+    protected void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
         if (!world.isClient()) {
-            // Motion sensor detection logic
+            world.scheduleBlockTick(pos, this, 5);
         }
+    }
+
+    @Override
+    protected void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        Box box = new Box(pos).expand(5.0);
+        List<LivingEntity> entities = world.getEntitiesOfClass(LivingEntity.class, box);
+        boolean detected = !entities.isEmpty();
+
+        if (state.get(POWERED) != detected) {
+            world.setBlockState(pos, state.with(POWERED, detected), Block.NOTIFY_LISTENERS);
+            world.updateNeighbors(pos, this);
+        }
+
+        world.scheduleBlockTick(pos, this, 5);
     }
 }
