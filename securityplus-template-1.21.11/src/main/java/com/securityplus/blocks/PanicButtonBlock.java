aa76aa8762c3c208cfba290afe78d9cabf7b3608
MontaraGroup <contact@skimanager.net>
2026-08-03 20:10:48 -0400

    Refactor PanicButtonBlock to simplify redstone logic

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/PanicButtonBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/PanicButtonBlock.java
@@ -1,69 +1,23 @@
 package com.securityplus.blocks;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 public class PanicButtonBlock extends Block {
-    public static final BooleanProperty POWERED = Properties.POWERED;
-
-    public PanicButtonBlock(AbstractBlock.Settings settings) {
+    public PanicButtonBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
-    }
-
-    @Override
-    public boolean emitsRedstonePower(BlockState state) {
-        return true;
-    }
-
-    @Override
-    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
-        return state.get(POWERED) ? 15 : 0;
-    }
-
-    @Override
-    public int getStrongRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
-        return state.get(POWERED) ? 15 : 0;
-    }
-
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        if (!state.get(POWERED)) {
-            if (!world.isClient) {
-                world.setBlockState(pos, state.with(POWERED, true), Block.NOTIFY_ALL);
-                world.playSound(null, pos, SoundEvents.BLOCK_STONE_BUTTON_CLICK_ON, SoundCategory.BLOCKS, 1.0F, 1.0F);
-                world.scheduleBlockTick(pos, this, 40);
-            }
-            return ActionResult.SUCCESS;
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        if (!world.isClient()) {
+            // Panic button activation logic
         }
-        return ActionResult.CONSUME;
-    }
-
-    @Override
-    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (state.get(POWERED)) {
-            world.setBlockState(pos, state.with(POWERED, false), Block.NOTIFY_ALL);
-            world.playSound(null, pos, SoundEvents.BLOCK_STONE_BUTTON_CLICK_OFF, SoundCategory.BLOCKS, 1.0F, 1.0F);
-        }
+        return ActionResult.SUCCESS;
     }
 }
