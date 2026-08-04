6e667f2ee7632c3f71634617afad52a619144737
MontaraGroup <contact@skimanager.net>
2026-08-03 20:06:21 -0400

    Update OwnableBlockEntity.java

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blockentity/OwnableBlockEntity.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blockentity/OwnableBlockEntity.java
@@ -3,10 +3,8 @@ package com.securityplus.blockentity;
 import com.securityplus.init.ModBlockEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtHelper;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.math.BlockPos;
 
@@ -19,28 +17,11 @@ public class OwnableBlockEntity extends BlockEntity {
         super(ModBlockEntities.OWNABLE_BLOCK_ENTITY, pos, state);
     }
 
-    public OwnableBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
-        super(type, pos, state);
-    }
-
-    public void setOwner(UUID uuid) {
-        this.ownerUuid = uuid;
-        this.markDirty();
-    }
-
-    public UUID getOwner() {
-        return this.ownerUuid;
-    }
-
-    public boolean isOwnedBy(PlayerEntity player) {
-        return this.ownerUuid != null && this.ownerUuid.equals(player.getUuid());
-    }
-
     @Override
     protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
         super.readNbt(nbt, registries);
         if (nbt.contains("Owner")) {
-            this.ownerUuid = NbtHelper.toUuid(nbt.get("Owner"));
+            this.ownerUuid = nbt.getUuid("Owner").orElse(null);
         }
     }
 
@@ -48,7 +29,23 @@ public class OwnableBlockEntity extends BlockEntity {
     protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
         super.writeNbt(nbt, registries);
         if (this.ownerUuid != null) {
-            nbt.put("Owner", NbtHelper.fromUuid(this.ownerUuid));
+            nbt.putUuid("Owner", this.ownerUuid);
         }
+    }
+
+    public UUID getOwnerUuid() {
+        return ownerUuid;
+    }
+
+    public void setOwner(PlayerEntity player) {
+        if (player != null) {
+            this.ownerUuid = player.getUuid();
+            markDirty();
+        }
+    }
+
+    public boolean isOwnedBy(PlayerEntity player) {
+        if (player == null) return false;
+        return ownerUuid != null && ownerUuid.equals(player.getUuid());
     }
 }
