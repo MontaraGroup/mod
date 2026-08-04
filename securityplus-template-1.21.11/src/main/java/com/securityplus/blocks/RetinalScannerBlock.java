bd78ca45b9b1d441ffa593c63c44c8e27edee7e4
MontaraGroup <contact@skimanager.net>
2026-08-03 20:09:37 -0400

    Refactor RetinalScannerBlock to remove BlockEntityProvider
    Removed BlockEntityProvider implementation and related methods. Simplified the RetinalScannerBlock class by removing unnecessary imports and properties.

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/RetinalScannerBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/RetinalScannerBlock.java
@@ -1,81 +1,23 @@
 package com.securityplus.blocks;
 
-import com.securityplus.blockentity.OwnableBlockEntity;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-public class RetinalScannerBlock extends Block implements BlockEntityProvider {
-    public static final BooleanProperty POWERED = Properties.POWERED;
-
-    public RetinalScannerBlock(AbstractBlock.Settings settings) {
+public class RetinalScannerBlock extends Block {
+    public RetinalScannerBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
-    }
-
-    @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new OwnableBlockEntity(pos, state);
-    }
-
-    @Override
-    public boolean emitsRedstonePower(BlockState state) {
-        return true;
-    }
-
-    @Override
-    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
-        return state.get(POWERED) ? 15 : 0;
-    }
-
-    @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
-        super.onPlaced(world, pos, state, placer, itemStack);
-        if (placer instanceof PlayerEntity player && world.getBlockEntity(pos) instanceof OwnableBlockEntity ownable) {
-            ownable.setOwner(player.getUuid());
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        if (!world.isClient()) {
+            // Retinal scanner verification logic
         }
-    }
-
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        if (!world.isClient) {
-            if (world.getBlockEntity(pos) instanceof OwnableBlockEntity ownable && ownable.isOwnedBy(player)) {
-                world.setBlockState(pos, state.with(POWERED, true), Block.NOTIFY_ALL);
-                world.playSound(null, pos, SoundEvents.BLOCK_NOTE_BLOCK_PLING.value(), SoundCategory.BLOCKS, 1.0F, 2.0F);
-                world.scheduleBlockTick(pos, this, 30);
-                return ActionResult.SUCCESS;
-            }
-        }
-        return ActionResult.CONSUME;
-    }
-
-    @Override
-    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (state.get(POWERED)) {
-            world.setBlockState(pos, state.with(POWERED, false), Block.NOTIFY_ALL);
-        }
+        return ActionResult.SUCCESS;
     }
 }
