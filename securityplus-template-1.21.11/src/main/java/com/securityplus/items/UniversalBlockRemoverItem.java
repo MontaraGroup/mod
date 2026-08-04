eafb93cc9480878a5ec44878cf17d0f0021a5a16
MontaraGroup <contact@skimanager.net>
2026-08-03 22:53:01 -0400

    Simplify block removal logic in UniversalBlockRemoverItem
    Removed OP permission check for block removal.

--- securityplus-template-1.21.11/src/main/java/com/securityplus/items/UniversalBlockRemoverItem.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/items/UniversalBlockRemoverItem.java
@@ -5,7 +5,6 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
@@ -33,13 +32,7 @@ public class UniversalBlockRemoverItem extends Item {
                 return ActionResult.SUCCESS;
             }
 
-            // Check OP permission on ServerPlayerEntity
-            boolean isOp = false;
-            if (player instanceof ServerPlayerEntity serverPlayer) {
-                isOp = serverPlayer.hasPermissionLevel(2);
-            }
-
-            boolean canRemove = ownable.isOwnedBy(player) || player.isCreative() || isOp;
+            boolean canRemove = ownable.isOwnedBy(player) || player.isCreative();
 
             if (canRemove) {
                 world.breakBlock(pos, true, player);
