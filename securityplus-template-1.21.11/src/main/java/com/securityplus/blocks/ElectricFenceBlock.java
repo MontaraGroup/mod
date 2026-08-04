834a6bf947a8131ba01c5ead9e317fab92dad7cf
MontaraGroup <contact@skimanager.net>
2026-08-03 20:10:05 -0400

    Update ElectricFenceBlock.java

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/ElectricFenceBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/ElectricFenceBlock.java
@@ -1,37 +1,20 @@
 package com.securityplus.blocks;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class ElectricFenceBlock extends Block {
-    public static final BooleanProperty POWERED = Properties.POWERED;
-
-    public ElectricFenceBlock(AbstractBlock.Settings settings) {
+    public ElectricFenceBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
-    }
-
-    @Override
-    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        if (!world.isClient() && world instanceof ServerWorld serverWorld && entity instanceof LivingEntity livingEntity) {
-            livingEntity.damage(serverWorld, world.getDamageSources().lightningBolt(), 2.0F);
-            livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 60, 0));
+    protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (!world.isClient()) {
+            entity.serverDamage(world.getDamageSources().lightningBolt(), 2.0f);
         }
     }
 }
