cd50037c13b1180824330af0879e059b74453008
MontaraGroup <contact@skimanager.net>
2026-08-03 20:05:40 -0400

    Update SecurityPlus.java

--- securityplus-template-1.21.11/src/main/java/com/securityplus/SecurityPlus.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/SecurityPlus.java
@@ -5,15 +5,20 @@ import com.securityplus.init.ModBlocks;
 import com.securityplus.init.ModCreativeTabs;
 import com.securityplus.init.ModItems;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SecurityPlus implements ModInitializer {
     public static final String MOD_ID = "securityplus";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        ModBlocks.registerBlocks();
-        ModItems.registerItems();
+        ModBlocks.registerModBlocks();
+        ModItems.registerModItems();
         ModBlockEntities.registerBlockEntities();
         ModCreativeTabs.registerCreativeTabs();
+
+        LOGGER.info("SecurityPlus initialized successfully!");
     }
 }
