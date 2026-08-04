65e343ba77d9bd4ac84466c0e01782f797acda19
MontaraGroup <contact@skimanager.net>
2026-08-03 22:25:55 -0400

    Refactor ModBlockEntities for block entity registration
    Refactor block entity registration to use final variables and simplify the registerBlockEntities method.

--- securityplus-template-1.21.11/src/main/java/com/securityplus/init/ModBlockEntities.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/init/ModBlockEntities.java
@@ -1,6 +1,5 @@
 package com.securityplus.init;
 
-import com.securityplus.SecurityPlus;
 import com.securityplus.blockentity.KeypadBlockEntity;
 import com.securityplus.blockentity.LockdownLeverBlockEntity;
 import com.securityplus.blockentity.OwnableBlockEntity;
@@ -12,34 +11,29 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModBlockEntities {
-    public static BlockEntityType<OwnableBlockEntity> OWNABLE_BLOCK_ENTITY;
-    public static BlockEntityType<KeypadBlockEntity> KEYPAD_BLOCK_ENTITY;
-    public static BlockEntityType<LockdownLeverBlockEntity> LOCKDOWN_LEVER_BLOCK_ENTITY;
-    public static BlockEntityType<SentryTurretBlockEntity> SENTRY_TURRET_BLOCK_ENTITY;
-
-    public static void registerBlockEntities() {
-        OWNABLE_BLOCK_ENTITY = Registry.register(
+    public static final BlockEntityType<OwnableBlockEntity> OWNABLE_BLOCK_ENTITY = Registry.register(
             Registries.BLOCK_ENTITY_TYPE,
-            Identifier.of(SecurityPlus.MOD_ID, "ownable_block_entity"),
+            Identifier.of("securityplus", "ownable_block_entity"),
             FabricBlockEntityTypeBuilder.create(OwnableBlockEntity::new, ModBlocks.REINFORCED_BLOCK).build()
-        );
+    );
 
-        KEYPAD_BLOCK_ENTITY = Registry.register(
+    public static final BlockEntityType<KeypadBlockEntity> KEYPAD_BLOCK_ENTITY = Registry.register(
             Registries.BLOCK_ENTITY_TYPE,
-            Identifier.of(SecurityPlus.MOD_ID, "keypad_block_entity"),
+            Identifier.of("securityplus", "keypad_block_entity"),
             FabricBlockEntityTypeBuilder.create(KeypadBlockEntity::new, ModBlocks.KEYPAD_BLOCK).build()
-        );
+    );
 
-        LOCKDOWN_LEVER_BLOCK_ENTITY = Registry.register(
+    public static final BlockEntityType<LockdownLeverBlockEntity> LOCKDOWN_LEVER_BLOCK_ENTITY = Registry.register(
             Registries.BLOCK_ENTITY_TYPE,
-            Identifier.of(SecurityPlus.MOD_ID, "lockdown_lever_block_entity"),
+            Identifier.of("securityplus", "lockdown_lever_block_entity"),
             FabricBlockEntityTypeBuilder.create(LockdownLeverBlockEntity::new, ModBlocks.LOCKDOWN_LEVER_BLOCK).build()
-        );
+    );
 
-        SENTRY_TURRET_BLOCK_ENTITY = Registry.register(
+    public static final BlockEntityType<SentryTurretBlockEntity> SENTRY_TURRET_BLOCK_ENTITY = Registry.register(
             Registries.BLOCK_ENTITY_TYPE,
-            Identifier.of(SecurityPlus.MOD_ID, "sentry_turret_block_entity"),
+            Identifier.of("securityplus", "sentry_turret_block_entity"),
             FabricBlockEntityTypeBuilder.create(SentryTurretBlockEntity::new, ModBlocks.SENTRY_TURRET_BLOCK).build()
-        );
-    }
+    );
+
+    public static void registerBlockEntities() {}
 }
