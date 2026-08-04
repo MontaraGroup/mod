eaf46db2ad8a02eb35f78031550645f969ab1e0d
MontaraGroup <contact@skimanager.net>
2026-08-03 20:08:45 -0400

    Update AlarmSirenBlock.java

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/AlarmSirenBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/AlarmSirenBlock.java
@@ -1,49 +1,19 @@
 package com.securityplus.blocks;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
 public class AlarmSirenBlock extends Block {
-    public static final BooleanProperty POWERED = Properties.POWERED;
-
-    public AlarmSirenBlock(AbstractBlock.Settings settings) {
+    public AlarmSirenBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
-    }
-
-    @Override
-    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
-        if (!world.isClient) {
-            boolean isPowered = world.isReceivingRedstonePower(pos);
-            if (isPowered != state.get(POWERED)) {
-                world.setBlockState(pos, state.with(POWERED, isPowered), Block.NOTIFY_ALL);
-                if (isPowered) {
-                    world.scheduleBlockTick(pos, this, 1);
-                }
-            }
-        }
-    }
-
-    @Override
-    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (state.get(POWERED)) {
-            world.playSound(null, pos, SoundEvents.BLOCK_NOTE_BLOCK_BELL.value(), SoundCategory.BLOCKS, 2.0F, 1.0F);
-            world.scheduleBlockTick(pos, this, 20);
+    protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
+        if (!world.isClient()) {
+            // Alarm trigger logic
         }
     }
 }
