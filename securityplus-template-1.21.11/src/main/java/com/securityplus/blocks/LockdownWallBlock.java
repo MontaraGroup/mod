2f77823cfeb96c761797166d74aa4d947b2759f9
MontaraGroup <contact@skimanager.net>
2026-08-04 11:22:32 -0400

    Implement lockdown functionality in LockdownWallBlock

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownWallBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownWallBlock.java
@@ -2,20 +2,44 @@ package com.securityplus.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.block.WireOrientation;
 import org.jetbrains.annotations.Nullable;
 
-public class LockdownWallBlock extends Block {
+public class LockdownWallBlock extends Block implements LockdownControllable {
+    public static final BooleanProperty POWERED = Properties.POWERED;
+
     public LockdownWallBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getStateManager().getDefaultState().with(POWERED, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(POWERED);
     }
 
     @Override
     protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, @Nullable WireOrientation wireOrientation, boolean notify) {
         if (!world.isClient()) {
-            // Lockdown wall state updates
+            boolean hasSignal = world.isReceivingRedstonePower(pos);
+            if (state.get(POWERED) != hasSignal) {
+                setLockdownState(world, pos, state, hasSignal);
+            }
+        }
+    }
+
+    @Override
+    public void setLockdownState(World world, BlockPos pos, BlockState state, boolean active) {
+        if (state.get(POWERED) != active) {
+            world.setBlockState(pos, state.with(POWERED, active), Block.NOTIFY_LISTENERS);
+            world.playSound(null, pos, SoundEvents.BLOCK_NETHERITE_BLOCK_HIT, SoundCategory.BLOCKS, 0.8F, 0.6F);
         }
     }
 }
