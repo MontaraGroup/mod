439c5945c7f7694f7781e25bde6e46afd96e4c17
MontaraGroup <contact@skimanager.net>
2026-08-03 21:01:25 -0400

    Enhance PanicButtonBlock with redstone and sound features
    Added functionality to the PanicButtonBlock to emit redstone power and trigger a sound when pressed. The button now changes state and sends a message to the player.

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/PanicButtonBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/PanicButtonBlock.java
@@ -3,21 +3,61 @@ package com.securityplus.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 public class PanicButtonBlock extends Block {
+    public static final BooleanProperty POWERED = Properties.POWERED;
+
     public PanicButtonBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getStateManager().getDefaultState().with(POWERED, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(POWERED);
+    }
+
+    @Override
+    protected boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
+
+    @Override
+    protected int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return state.get(POWERED) ? 15 : 0;
     }
 
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        if (!world.isClient()) {
-            // Panic button activation logic
+        if (!world.isClient() && !state.get(POWERED)) {
+            world.setBlockState(pos, state.with(POWERED, true), Block.NOTIFY_LISTENERS);
+            world.updateNeighbors(pos, this);
+            world.playSound(null, pos, SoundEvents.BLOCK_NOTE_BLOCK_BELL.value(), SoundCategory.BLOCKS, 2.0F, 2.0F);
+            player.sendMessage(Text.literal("§c§lPANIC BUTTON PRESSED!"), true);
+            world.scheduleBlockTick(pos, this, 30);
         }
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    protected void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (state.get(POWERED)) {
+            world.setBlockState(pos, state.with(POWERED, false), Block.NOTIFY_LISTENERS);
+            world.updateNeighbors(pos, this);
+        }
     }
 }
