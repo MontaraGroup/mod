5fb956a76ea699d6f64a8cb13d9fe7e45acb2899
MontaraGroup <contact@skimanager.net>
2026-08-03 20:10:19 -0400

    Refactor LockdownWallBlock by removing POWERED property
    Removed POWERED property and related functionality.

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownWallBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownWallBlock.java
@@ -1,34 +1,19 @@
 package com.securityplus.blocks;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class LockdownWallBlock extends Block {
-    public static final BooleanProperty POWERED = Properties.POWERED;
-
-    public LockdownWallBlock(AbstractBlock.Settings settings) {
+    public LockdownWallBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
-    }
-
-    @Override
-    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
-        if (!world.isClient) {
-            boolean isPowered = world.isReceivingRedstonePower(pos);
-            if (isPowered != state.get(POWERED)) {
-                world.setBlockState(pos, state.with(POWERED, isPowered), Block.NOTIFY_ALL);
-            }
+    protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
+        if (!world.isClient()) {
+            // Lockdown wall state updates
         }
     }
 }
