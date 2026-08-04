a7697ffc32cf8e59f0c731e615f1f7529c85bc48
MontaraGroup <contact@skimanager.net>
2026-08-03 21:01:10 -0400

    Update LockdownShutterBlock.java

--- securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownShutterBlock.java
+++ securityplus-template-1.21.11/src/main/java/com/securityplus/blocks/LockdownShutterBlock.java
@@ -2,18 +2,35 @@ package com.securityplus.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class LockdownShutterBlock extends Block {
+    public static final BooleanProperty POWERED = Properties.POWERED;
+
     public LockdownShutterBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getStateManager().getDefaultState().with(POWERED, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(POWERED);
     }
 
     @Override
     protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
         if (!world.isClient()) {
-            // Shutter opening/closing logic
+            boolean hasSignal = world.isReceivingRedstonePower(pos);
+            if (state.get(POWERED) != hasSignal) {
+                world.setBlockState(pos, state.with(POWERED, hasSignal), Block.NOTIFY_LISTENERS);
+                world.playSound(null, pos, SoundEvents.BLOCK_ANVIL_USE, SoundCategory.BLOCKS, 0.5F, 1.2F);
+            }
         }
     }
 }
